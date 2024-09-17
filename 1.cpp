e80fe38f01687e15516e2511d5b3ebf9ec73894f
davi <davimelovas61@gmail.com>
2024-09-17 00:32:40 -0300

    First commit

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nome;
+    int idade;
+    float altura;
+
+    //cin - Entrada de dados
+    cin>>nome>>idade>>altura;
+
+    cout<<nome<<" tem "<<idade<<" anos e mede "<<altura<<" metros.\n";
+
+    return 0;
+}
